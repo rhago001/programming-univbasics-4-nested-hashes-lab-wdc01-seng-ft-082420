97b1fe42a714c5ca9763ef61bea7ba4e38d090d9
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-05 08:02:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,6 +132,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash.merge[:dennis_ritchie][:languages]=["Assembly"]
+programmer_hash.merge![:dennis_ritchie][:languages]=["Assembly"]
 programmer_hash
 end
