61d97c486bf74a18b6dd5cc1dd67765877812372
Rahel Hagos <hagos.rahelh@gmail.com>
2020-08-05 06:48:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -74,8 +74,8 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",
-        :languages => ["LISP", "C"]}
+programmer_hash(:yukihiro_matsumoto=> {:known_for => "Ruby",
+        :languages => ["LISP", "C"]})
 
 	programmer_hash = 
  		{
